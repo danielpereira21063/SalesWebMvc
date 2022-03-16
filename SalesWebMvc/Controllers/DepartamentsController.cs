1848a45a3f6a4769e9dbdbfc4f98afffb05be348
Daniel Sanches <danielsanches6301@gmail.com>
2022-03-16 09:02:46 -0300

    Model View Departament

---
+++ SalesWebMvc/Controllers/DepartamentsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+using System.Collections.Generic;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartamentsController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departament> departaments = new List<Departament>();
+
+            departaments.Add(new Departament { Id = 1, Name = "Eletronics" });
+            departaments.Add(new Departament { Id = 2, Name = "Fashion" });
+
+            return View(departaments);
+        }
+    }
+}
