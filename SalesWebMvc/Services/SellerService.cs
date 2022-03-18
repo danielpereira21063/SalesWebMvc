61f1c25f602e511d5596acdc41ab70b82b787292
Daniel Sanches <danielsanches6301@gmail.com>
2022-03-18 19:34:13 -0300

    eager loading

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -1,4 +1,5 @@
-﻿using SalesWebMvc.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMvc.Data;
 using SalesWebMvc.Models;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,9 +20,24 @@ namespace SalesWebMvc.Services
             return _context.Seller.ToList();
         }
 
+        public Seller FindById(int id)
+        {
+            return _context.Seller.Include(x => x.Departament).FirstOrDefault(x => x.Id == id);
+        }
         public void Insert(Seller seller)
         {
             _context.Add(seller);
+            _context.SaveChanges();
+        }
+
+        public void Remove(int id)
+        {
+            var seller = _context.Seller.FirstOrDefault(x => x.Id == id);
+            if (seller == null)
+            {
+                return;
+            }
+            _context.Seller.Remove(seller);
             _context.SaveChanges();
         }
     }
